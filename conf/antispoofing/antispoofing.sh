29305f8103e9b99718349dd4de9f35cd5e6fabf5
José Domingo <josedom24@gmail.com>
2017-04-04 12:18:24 +0200

    Port security

--- conf/antispoofing/antispoofing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-nova remove-secgroup cliente default
-IP=$(nova show cliente|grep demo-net| awk '{print $5}')
-NET_P1=$(neutron port-list|grep ${IP::-1}|awk '{print $2}')
-NET_P2=$(neutron port-list|grep '192.168.1.1"'|awk '{print $2}')
-
-neutron port-update --no-security-groups --port-security-enabled=False $NET_P1
-neutron port-update --no-security-groups --port-security-enabled=False $NET_P2
